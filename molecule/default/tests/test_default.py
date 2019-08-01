79932a8f1d4544b936460350fbaee64795bf6c15
amit642 <amitb5639@gmail.com>
2019-08-01 16:31:10 +0530

    test cases

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,37 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_grafana_is_installed(host):
+    grafana = host.package("grafana")
+    assert grafana.is_installed
 
+
+def test_config_file(host):
+    f = host.file('/etc/grafana/grafana.ini')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.is_file
+
+
+def test_grafana_dashboard_dir(host):
+    d = host.file('/var/lib/grafana/dashboards')
+    assert d.exists
+    assert d.is_directory
+    assert d.user == "grafana"
+    assert d.group == "grafana"
+    assert d.mode == '0755'
+
+
+def test_grafana_running_and_enabled(host):
+    os = host.system_info.distribution
+    if os == '16':
+        grafana = host.service("grafana-server")
+        assert grafana.is_running
+        assert grafana.is_enabled
+    if os == '7':
+        grafana = host.service("grafana-server")
+        assert grafana.is_running
+        assert grafana.is_enabled
+
+
+def test_grafana_is_listening(host):
+    assert host.socket('tcp://127.0.0.1:3000').is_listening
